eee65187669f459943fc8ecc7df3c2b767b2518e
Santosh Marigowda <santosharakere@gmail.com>
2020-04-09 11:04:28 +0100

    using FormControl to observe the change in value of an input. Also using ShadowDOM

--- projects/dom-events-to-observables/src/app/app.component.ts
+++ projects/dom-events-to-observables/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewEncapsulation,
+} from '@angular/core';
+// import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class AppComponent {
-  title = 'dom-events-to-observables';
+export class AppComponent implements AfterViewInit {
+  // title = 'dom-events-to-observables';
+  searchInput = new FormControl('');
+  ngAfterViewInit() {
+    this.searchInput.valueChanges.pipe(debounceTime(500)).subscribe(stock => this.getStockQuoteFromServer(stock));
+  }
+  getStockQuoteFromServer(stock: string) {
+    console.log('--- stock symbol entered is ---');
+    console.log(stock);
+    console.log(`The price of ${stock} is ${(100 * Math.random()).toFixed(4)}`);
+  }
 }
